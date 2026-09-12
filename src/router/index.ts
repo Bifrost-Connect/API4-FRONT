439b5fbbf92deaa6bda5180dc8d5217dcd1d819a
NiuanSouza <niuan.spolid@hotmail.com>
2026-09-11 22:25:51 -0300

    feat(auth): implementa telas de login e dashboard (#D1)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardView from '../views/DashboardView.vue'
 import StyleguideView from '../views/StyleguideView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/styleguide',
       name: 'styleguide',
       component: StyleguideView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
   ],
 })
